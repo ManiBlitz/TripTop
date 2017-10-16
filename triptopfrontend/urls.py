495cda832b9f02615e71629d653055d301080b45
Kevin Tiba <tibakevin@gmail.com>
2017-10-15 23:51:17 -0400

    Added links for menu

--- triptopfrontend/urls.py
+++ triptopfrontend/urls.py
@@ -4,12 +4,6 @@ from django.conf.urls.static import static
 from triptopfrontend import views
 
 urlpatterns = [
-    #url(r'^$',
-     #   dispatch_by_user(
-      #          views.Registered_User.as_view(),
-       #         views.UnRegistered_User.as_view()),
-       # name='index'),
-     # ...
 
     url(r'^$', views.HomePageView.as_view()),
     url('itinerary', views.ItineraryBuilderPageView.as_view()),
