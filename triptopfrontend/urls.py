37b53fd270f30ca35c6aed9e770b924584e0eaef
Michael <mike.bourgault@gmail.com>
2017-11-26 17:28:42 -0500

    Set Urls/Views

--- triptopfrontend/urls.py
+++ triptopfrontend/urls.py
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-# helloworld/urls.py
-from django.conf.urls import url
-from django.conf.urls.static import static
-from triptopfrontend import views
-
-urlpatterns = [
-
-    url(r'^$', views.HomePageView.as_view()),
-    url('itinerary', views.ItineraryBuilderPageView.as_view()),
-    url('login', views.LoginPageView.as_view()),
-    url('profile', views.ProfilePageView.as_view())
-]
-
-
-=======
 # helloworld/urls.py
 from django.conf.urls import url
 from django.conf.urls.static import static
@@ -28,4 +12,3 @@ urlpatterns = [
 ]
 
 
->>>>>>> 7d24be8c984cb632a45a78b98928eb4b36907850
