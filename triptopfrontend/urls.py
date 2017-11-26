0cb30b6a50537abef7feed846cf21b924fea0d72
Kevin Tiba <tibakevin@gmail.com>
2017-11-26 17:42:04 -0500

    Cleaned views and url files

--- triptopfrontend/urls.py
+++ triptopfrontend/urls.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 # helloworld/urls.py
 from django.conf.urls import url
 from django.conf.urls.static import static
@@ -13,19 +13,3 @@ urlpatterns = [
 ]
 
 
-=======
-# helloworld/urls.py
-from django.conf.urls import url
-from django.conf.urls.static import static
-from triptopfrontend import views
-
-urlpatterns = [
-
-    url(r'^$', views.HomePageView.as_view()),
-    url(r'^itinerary/$', views.ItineraryBuilderPageView.as_view()),
-    url(r'^login/$', views.LoginPageView.as_view()),
-    url(r'^profile/$', views.ProfilePageView.as_view())
-]
-
-
->>>>>>> 7d24be8c984cb632a45a78b98928eb4b36907850
--- triptopfrontend/views.py
+++ triptopfrontend/views.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
  # triptop/views.py
 from django.shortcuts import render
 from django.views.generic import TemplateView
@@ -23,31 +23,3 @@ class ProfilePageView(TemplateView):
         return render(request, 'myProfile.html', context=None)
 
 
-
-=======
- # triptop/views.py
-from django.shortcuts import render
-from django.views.generic import TemplateView
-
-
-
-# Create your views here.
-class HomePageView(TemplateView):
-    def get(self, request, **kwargs):
-        return render(request, 'index.html', context=None)
-
-class ItineraryBuilderPageView(TemplateView):
-    def get(self, request, **kwargs):
-        return render(request, 'itinBuilder.html', context=None)
-
-class LoginPageView(TemplateView):
-    def get(self, request, **kwargs):
-        return render(request, 'login.html', context=None)
-
-class ProfilePageView(TemplateView):
-    def get(self, request, **kwargs):
-        return render(request, 'myProfile.html', context=None)
-
-
-
->>>>>>> 7d24be8c984cb632a45a78b98928eb4b36907850
